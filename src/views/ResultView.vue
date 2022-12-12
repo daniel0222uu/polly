5ad5c18f0964ecdb0510c61752d90be1cb160878
daniel0222u <daniel.ceoca.0222@student.uu.se>
2022-12-12 15:08:55 +0100

    Detta är så att man nu spelar i resultView med lite CSS fix.

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -5,6 +5,15 @@
   </div>
   <BarsComponent v-bind:data="submittedAnswers"/>
   <br>
+  <div>
+    <select v-model="selectedDeck" name="drinks" required>
+      <option value="" disabled selected hidden>Choose a drink</option>
+      <option v-for="drink in selectorList" v-bind:key="drink">{{ drink }}</option>
+      <!--  <option v-for="fraga in selectorList" :value="fraga">{{ fraga.id }}<option> -->
+    </select> {{selectedDeck}}
+    {{selectorList}}
+    <button @click="loadDeck">Load deck</button>
+  </div>
 
   <div id="questionDiv" @click="questionPress" v-if="answerButtonBool==false">
     {{questionObject.questionArray[questionPosition]}}
@@ -25,6 +34,11 @@ import BarsComponent from '@/components/BarsComponent.vue';
 import io from 'socket.io-client';
 const socket = io();
 
+let listToFill = [];
+for (var i =0, len = localStorage.length; i< len; ++i ) {
+  listToFill.push(localStorage.key(i));
+}
+
 export default {
   name: 'ResultView',
   components: {
@@ -32,6 +46,8 @@ export default {
   },
   data: function () {
     return {
+      selectorList:listToFill,
+      selectedDeck: "",
       question: "",
       submittedAnswers: {
       },
@@ -61,6 +77,15 @@ export default {
         this.questionPosition = this.questionPosition + 1;
       }
       this.answerButtonBool = false;
+    },
+    loadDeck: function(){
+      console.log("du klickade på en knapp med loadDeck()")
+      let myObj_deserialized = JSON.parse(localStorage.getItem(this.selectedDeck));
+      console.log(myObj_deserialized);
+      //this.questionObject = myObj_deserialized;
+      //this.answerField = this.questionObject.answerArray[this.questionIndex];
+      //this.questionField = this.questionObject.questionArray[this.questionIndex];
+      this.questionObject = myObj_deserialized;
     },
   },
   created: function () {
