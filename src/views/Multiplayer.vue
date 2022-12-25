e5ecf50b14cdf45854c9f36b891d1a2f0a97ed89
Nima Khaksar <n.khaksar@student.tudelft.nl>
2022-12-25 15:25:20 +0100

    Multiplayer testing

--- src/views/Multiplayer.vue
+++ src/views/Multiplayer.vue
@@ -2,42 +2,48 @@
   <body>
   <div>
     <h1>"Multiplayer mode"</h1>
+    <!--See the interface -->
+    <canvas reference  = "game" width="640" height="480"
+            style="border: 2px solid black">
+    </canvas>
   </div>
   </body>
 </template>
 
-
 <script>
-
 //Importing packages:
 import io from 'socket.io-client';
 import Decks from "../assetts/Decks.json";
-const socket = io();
+//const socket = io();
 console.log(Decks);
 
 export default {
   name: 'Multiplayer',
   data() {
-    return{
-      socket:{},
-      context:{},
+    return {
+      socket: {},
+      context: {},
+
+      //The coordinates for the deck to be placed at:
+      position : {
+        x: 0,
+        y: 0
+      }
     }
   },
+
+  //Connection to the server:
   created() {
     this.socket = io("http://localhost:8080");
   },
 
-
-  /*Kladd infor nasta gng:
+  //  Want to display the decks with their content
   mounted() {
-    //this.context = this.$refs.
-
-
-  }*/
+    this.context = this.$refs.decks.getContext("2d");
+    this.socket.on("message") //Check the connection.
+    this.context.fill(this.position.x, this.position.y)
+  }
 }
-
-
-
 
 </script>
 
@@ -52,7 +58,4 @@ body{
   font-size: medium;
   alignment: center;
 }
-
-
-
 </style>
