2261a4ccc9c1385daed3a7d963cadcce10bb8c88
daniel0222u <daniel.ceoca.0222@student.uu.se>
2022-12-25 20:45:28 +0100

    Enkel kod bara för att kunna lägga lite snyggare bakgrund från nerladdad bild. Säger inte vi ska ha den här bakgrunden. Bara för att visa

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Island+Moments&family=Kanit:ital,wght@1,300&family=Mukta:wght@200&display=swap');
 :root {
 	--foreground-color: red;
 	--background-color: green;
@@ -13,10 +14,15 @@ body {
   justify-content: center;
   margin: 0;
   padding: 0;
-  background-color: lightblue;
+  background-image: url("../public/img/backgroundPicture.png");
+  background-opacity: 0.5;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  font-family: 'Kanit', sans-serif;
 }
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
