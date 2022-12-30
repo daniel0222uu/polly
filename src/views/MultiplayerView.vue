83cbae0ed9e93911eb9cbcf5291526b47747032e
Nima Khaksar <n.khaksar@student.tudelft.nl>
2022-12-30 13:35:37 +0100

    Kodade grunden för att kommentarer från användaren ska sparas i en jsonfil (ej likes)

--- src/views/MultiplayerView.vue
+++ src/views/MultiplayerView.vue
@@ -1,21 +1,32 @@
 <template>
-
-
   <body>
   <div>
-{{inviteInformation}}
+    {{inviteInformation}}
   </div>
   <div id="wrapperDiv">
     <div id="horizontalContent">
 
       <p  style="font-size: 30px; font-weight: bolder"> CHOOSE YOUR NAME</p>
       <div v-if="!joinedBoolean">
-      <input v-model="name" type="text" />
+        <input v-model="name" type="text" />
         <button @click="startPlaying" > Start playing! </button>
       </div>
       <div>
         <FlashcardView v-bind:questionProp="myObj_deserialized" @nextClick="onClickChild" @previousClick="onClickChild" ></FlashcardView>
       </div>
+
+      <!-- Kommentarer start -->
+
+      <div id = "app">
+        <input type="text" v-model="todoName" @keyup.enter="addTodo">
+        <ul>
+          <li v-for="todo of todos" :key="todo.id">
+            {{todo.name}}
+          </li>
+        </ul>
+      </div>
+
+      <!-- kommentarer end -->
 
       <div style="background-color: white">
         <label>
@@ -50,11 +61,15 @@
 
 <script>
 import Decks from "../assetts/Decks.json";
+
+// Importerar axios!
+import axios from "axios";
+const baseURL = "http://localhost:8080"
+// Comments end
+
 import io from "socket.io-client";
 import FlashcardView from "@/components/FlashcardComponent";
 const socket = io();
-
-
 console.log(Decks);
 export default {
   name: "MultiplayerView",
@@ -75,49 +90,77 @@ export default {
       questionObject:   {"id": "Sveriges huvudstäder",
         "questionArray": ["Sverige", "Norge", "Finland", "Danmark"],
         "answerArray": ["Sthlm", "Oslo", "Helsingfors", "CBH"]},
-    }
+
+      // Comments start:
+      todos: [],
+      todoName: ''
+      // Comments end
+    };
   },
   created: function() {
     this.myObj_deserialized = this.questionObject;
     socket.on("dataUpdate", playersActive => {
-        this.players = playersActive;
+      this.players = playersActive;
     });
     /*socket.on("requestReceive", inviteInformation => {
       this.inviteInformation = inviteInformation;
     });*/
-  },
-  methods:
-  {
-    startPlaying: function(){
-      this.joinedBoolean = true;
-      socket.emit("startPlaying", {name: this.name, score: this.questionIndex});
-    },
-    onClickChild: function(value){
-      this.questionPosition = value;
-      console.log("parent has", this.questionPosition);
-      socket.emit("numberProgress", {name: this.name, score: this.questionPosition});
-    },
-    sendRequest: function(playerToRequest){
-      this.createPoll();
-      socket.emit('playRequest', {
-        requester: this.name,
-        receiver: playerToRequest,
-        lobbyID: this.lobbyId
-      });
-      socket.emit('lobbyObject', {
-        lobbyID: this.lobbyId,
-        playersInLobby: []
-      });
-    },
-    createPoll: function () { //ett bättre namn hade varit createLobby, men jag är lat
-      this.lobbyId = Math.floor(Math.random()*1000000 + 100000);
-      socket.emit("createPoll", {pollId: this.lobbyId, lang: this.lang });
-    },
-    joinLobby: function(){
-      socket.emit("joinLobby", {lobbyID: this.lobbyId, name: this.name});
-      this.navigate();
+
+
+    /*Kommentarer ang jsonfil*/
+    try{
+      //const res = await axios.get("http://localhost:8080");
+      const res = axios.get("");
+      this.todos = res.data();
     }
+    catch (e) {
+      console.error(e);
+    }
+    //End comment
+
   },
+
+  methods:
+      {
+        startPlaying: function(){
+          this.joinedBoolean = true;
+          socket.emit("startPlaying", {name: this.name, score: this.questionIndex});
+        },
+        onClickChild: function(value){
+          this.questionPosition = value;
+          console.log("parent has", this.questionPosition);
+          socket.emit("numberProgress", {name: this.name, score: this.questionPosition});
+        },
+        sendRequest: function(playerToRequest){
+          this.createPoll();
+          socket.emit('playRequest', {
+            requester: this.name,
+            receiver: playerToRequest,
+            lobbyID: this.lobbyId
+          });
+          socket.emit('lobbyObject', {
+            lobbyID: this.lobbyId,
+            playersInLobby: []
+          });
+        },
+        createPoll: function () { //ett bättre namn hade varit createLobby, men jag är lat
+          this.lobbyId = Math.floor(Math.random()*1000000 + 100000);
+          socket.emit("createPoll", {pollId: this.lobbyId, lang: this.lang });
+        },
+        joinLobby: function(){
+          socket.emit("joinLobby", {lobbyID: this.lobbyId, name: this.name});
+          this.navigate();
+        },
+
+        // Created a method to store the API
+        async addTodo(){
+          const res = await axios.post(baseURL, {name:this.todoName});
+          this.todos = [...this.todos, res.data];
+          this.todoName = '';
+        },
+        // End comment
+
+      },
   watch: {
     inviteInformation: function(){
       console.log("inviteInformation is", this.inviteInformation)
@@ -125,7 +168,7 @@ export default {
         var inviteInfo = this.inviteInformation[i];
         if(inviteInfo.receiver === this.name){
           console.log("receiver is me and im invited by ", inviteInfo.requester,
-          "the lobby created is",inviteInfo.lobbyID);
+              "the lobby created is",inviteInfo.lobbyID);
         }
       }
     }
@@ -155,6 +198,4 @@ export default {
   max-width: 30%;
   border: 1px solid black;
 }
-
-
 </style>
